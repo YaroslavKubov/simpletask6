5fa44572abd4ff07f65b78a027eecd80dece1a2a
YaroslavKubov <yaroslavkubov@gmail.com>
2025-05-13 03:25:57 +0300

    Добавьте файлы проекта.

---
+++ simpletask6/simpletask6.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    float P, R;
+    int T;
+
+    std::cout << "Enter the amount : ";
+    std::cin >> P;
+
+    std::cout << "Enter the loan period in years : ";
+    std::cin >> T;
+
+    std::cout << "Enter the interest rate : ";
+    std::cin >> R;
+
+    float I = (P * T * R) / 100;
+
+    std::cout << fixed << setprecision(2);
+    std::cout << "\nSimple interest (float): " << I << endl;
+    std::cout << "Simple interest (int): " << static_cast<int>(I) << endl;
+
+    return 0;
+}
